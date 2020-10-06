2fbadf4f8e23c3a5738f529651ef159a8882967d
stefan.pogonaru <stefan.pogonaru@TS1340>
2020-10-06 10:25:51 +0300

    pupik

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,33 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import {cars, ICar} from './Demo';
-import './App.css';
-import CarItem from './CarItem';
-import axios from 'axios';
-
-interface IProps {
-  id: number;
-  name: string;
-}
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import { cars, ICar } from "./Demo";
+import "./App.css";
+import CarItem from "./CarItem";
+import axios from "axios";
+import "semantic-ui-css/semantic.min.css";
+import { Header, Icon, List } from "semantic-ui-react";
 
 function App() {
   const [values, setValues] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:5000/api/values").then(response => {
-      setValues(response.data)
-    })
-  },[])
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      setValues(response.data);
+    });
+  }, []);
   return (
-    <div className="App"> 
-      <ul>
-        {values.map((value:IProps) =>  
-          <div key={value.id}>
-            <h1>{value.id}</h1>
-            <h2>{value.name}</h2>
-          </div>
-        )
-      }
-      </ul>
+    <div className="App">
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {values.map((value: any) => (
+          <List.Item key={value.id}>{value.name}</List.Item>
+        ))}
+      </List>
     </div>
   );
 }
